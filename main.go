cb8e607e0d3e39e132ee83669b9939883dfd1882
linyj <linyongjie@rayvision.com>
2021-07-16 18:55:35 +0800

    add code

--- main.go
+++ main.go
@@ -1,15 +1,108 @@
 package main
 
 import (
+	"asyncClient/common/untils"
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"gopkg.in/ini.v1"
+	"net/http"
 	"os"
 	"path"
-	"gopkg.in/ini.v1"
-	"asyncClient/common/untils"
+	"time"
 )
 
+const (
+	SERVER_ERROR    = 1000 // 系统错误
+	NOT_FOUND       = 1001 // 401错误
+	UNKNOWN_ERROR   = 1002 // 未知错误
+	PARAMETER_ERROR = 1003 // 参数错误
+	AUTH_ERROR      = 1004 // 错误
+)
+
+type HandlerFunc func(c *gin.Context) error
+
+// api错误的结构体
+type APIException struct {
+	Code      int    `json:"-"`
+	ErrorCode int    `json:"error_code"`
+	Msg       string `json:"msg"`
+	Request   string `json:"request"`
+}
+
+// 实现接口
+func (e *APIException) Error() string {
+	return e.Msg
+}
+func newAPIException(code int, errorCode int, msg string) *APIException {
+	return &APIException{
+		Code:      code,
+		ErrorCode: errorCode,
+		Msg:       msg,
+	}
+}
+
+// 500 错误处理
+func ServerError() *APIException {
+	return newAPIException(http.StatusInternalServerError, SERVER_ERROR, http.StatusText(http.StatusInternalServerError))
+}
+
+// 404 错误
+func NotFound() *APIException {
+	return newAPIException(http.StatusNotFound, NOT_FOUND, http.StatusText(http.StatusNotFound))
+}
+
+// 未知错误
+func UnknownError(message string) *APIException {
+	return newAPIException(http.StatusForbidden, UNKNOWN_ERROR, message)
+}
+
+// 参数错误
+func ParameterError(message string) *APIException {
+	return newAPIException(http.StatusBadRequest, PARAMETER_ERROR, message)
+}
+
+func wrapper(handler HandlerFunc) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		var (
+			err error
+		)
+		err = handler(c)
+		if err != nil {
+			var apiException *APIException
+			if h, ok := err.(*APIException); ok {
+				apiException = h
+			} else if e, ok := err.(error); ok {
+				if gin.Mode() == "debug" {
+					// 错误
+					apiException = UnknownError(e.Error())
+				} else {
+					// 未知错误
+					apiException = UnknownError(e.Error())
+				}
+			} else {
+				apiException = ServerError()
+			}
+			apiException.Request = c.Request.Method + " " + c.Request.URL.String()
+			c.JSON(apiException.Code, apiException)
+			return
+		}
+	}
+}
+
+func user(c *gin.Context) error {
+	// TODO 逻辑判断
+	return ParameterError("userId传参有误")
+}
+
+func HandleNotFound(c *gin.Context) {
+	handleErr := NotFound()
+	handleErr.Request = c.Request.Method + " " + c.Request.URL.String()
+	c.JSON(handleErr.Code, handleErr)
+	return
+}
+
 func main() {
-	//dir,_ := os.Getwd()
 	projectDir := untils.GetProjectDirectory()
 	configIni := path.Join(projectDir, "conf", "config.ini")
 	fmt.Println("当前路径：", projectDir)
@@ -20,4 +113,24 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Println(cfg.Section("listen").Key("host"))
+
+	// zap.NewDevelopment 格式化输出
+	logger, _ := zap.NewDevelopment()
+	defer logger.Sync()
+	logger.Info("无法获取网址",
+		zap.String("url", "http://www.baidu.com"),
+		zap.Int("attempt", 3),
+		zap.Duration("backoff", time.Second),
+	)
+
+	r := gin.Default()
+	r.NoMethod(HandleNotFound)
+	r.NoRoute(HandleNotFound)
+	//r.GET("/ping", func(c *gin.Context) {
+	//	c.JSON(200, gin.H{
+	//		"message": "pong",
+	//	})
+	//})
+	r.GET("/ping", wrapper(user))
+	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
 }
